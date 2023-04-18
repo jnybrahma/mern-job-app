ab847c7e2417a033eaabfd13426b3cc22300acb6
Brahma, Jnyananjoy <jnybrahma@gmail.com>
2023-04-18 19:40:46 +1000

    added

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,3 +1,15 @@
+import { useState, useEffect } from "react";
+import { Logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterPage";
+
+const initialState ={
+  name: '',
+  email: '',
+  password: '',
+  isMember: true
+
+}
+
 const Register = () => {
   return (
     <h1>
