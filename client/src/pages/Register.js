057ac0b938bc233ef535011a919db189b1bcda2f
Brahma, Jnyananjoy <jnybrahma@gmail.com>
2023-04-19 19:43:27 +1000

    added

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,22 +1,59 @@
 import { useState, useEffect } from "react";
-import { Logo } from "../components";
+import { Logo , FormRow , Alert } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
 
 const initialState ={
   name: '',
   email: '',
   password: '',
-  isMember: true
+  isMember: true,
+  showAlert: false,
 
 }
 
 const Register = () => {
+  const [values, setValues ] = useState(initialState);
+  // global state and useNavigate
 
-  
+  const handleChange = (e) => {
+    console.log(e.target)
+  }
+
+  const onSubmit = (e) =>{
+    e.preventDefault()
+    console.log(e.target)
+  }
   return (
-    <h1>
-      Register
-    </h1>
+    <Wrapper className='full-page'>
+      <form className='form' onSubmit={onSubmit}>
+        <Logo/>
+        <h3> Login </h3>
+        {values.showAlert && <Alert/>}
+        {/* name input   */}
+       <FormRow 
+        type='text'
+        name='name'
+        value={values.name}
+        handleChange={handleChange}
+        />
+           {/* email input   */}
+       <FormRow 
+        type='email'
+        name='email'
+        value={values.email}
+        handleChange={handleChange}
+        />
+           {/* password input    */}
+       <FormRow 
+        type='password'
+        name='password'
+        value={values.password}
+        handleChange={handleChange}
+        />
+      <button type='submit' className='btn btn-block'> submit</button>
+      </form>
+    
+    </Wrapper>
   )
 }
 
