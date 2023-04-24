f6f6a3cb56c0d566f561f34a8bf593cf5a9848bb
Brahma, Jnyananjoy <jnybrahma@gmail.com>
2023-04-24 19:31:37 +1000

    added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,9 +1,22 @@
 import User from "../models/User.js"
 import { StatusCodes } from "http-status-codes"
 
+class CustomAPIError extends Error {
+    constructor(message) {
+        super(message)
+        this.statusCode = StatusCodes.BAD_REQUEST
+    }
+}
+
 const register = async(req, res) => {
 
-    const user = await User.create(req.body)
+    const { name, email, password } = req.body
+
+    if (!name || !email || !password) {
+        throw new CustomAPIError('please provide all values')
+    }
+
+    const user = await User.create({ name, email, password })
     return res.status(StatusCodes.CREATED).json({ user })
 
 
