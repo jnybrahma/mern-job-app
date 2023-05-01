3bee360a5e71793281fda784aec85c08904204ad
Brahma, Jnyananjoy <jnybrahma@gmail.com>
2023-05-01 19:07:50 +1000

    added

--- controllers/authController.js
+++ controllers/authController.js
@@ -50,8 +50,27 @@ const login = async(req, res) => {
     //res.send('login user')
 }
 const updateUser = async(req, res) => {
+    const { email, name, lastName, location } = req.body;
 
-    res.send('Update user')
+    if (!email || !name || !lastName || !location) {
+        throw new BadRequestError('Please provide all values')
+    }
+
+    const user = await User.findOne({ _id: req.user.userId });
+
+    user.email = email
+    user.name = name
+    user.lastName = lastName
+    user.location = location
+
+    await user.save()
+
+    const token = user.createJWT()
+
+    res.status(StatusCodes.OK).json({ user, token, location: user.location })
+        //console.log(req.user)
+        //res.send('Update user')
+
 }
 
 export { register, login, updateUser }
--- middleware/auth.js
+++ middleware/auth.js
@@ -13,7 +13,9 @@ const auth = async(req, res, next) => {
     const token = authHeader.split(' ')[1]
     try {
         const payload = jwt.verify(token, process.env.JWT_SECRET)
-        req.user = payload
+            //req.user = payload
+            //console.log(payload)
+        req.user = { userId: payload.userId }
         next()
     } catch (error) {
         throw new UnAuthenticatedError('Authentication invalid')
