caa638956bb0389be23799bfe1577df75637469b
Brahma, Jnyananjoy <jnybrahma@gmail.com>
2023-05-15 18:55:13 +1000

    added

--- client/src/components/StatsItem.js
+++ client/src/components/StatsItem.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Wrapper from '../assets/wrappers/StatItem'
 
-const StatsItem = () => {
+const StatsItem = ({count, title,icon,color, bcg}) => {
   return (
-    <div>
-      <h1> Single Stat item </h1>
-    </div>
+    <Wrapper>
+      <header>
+      <span className='count'>{count}</span> 
+      <div className='icon'>{icon}</div> 
+      </header>
+      <h5 className='title'>{title}</h5>
+    </Wrapper>
   )
 }
 
--- client/src/pages/dashboard/Stats.js
+++ client/src/pages/dashboard/Stats.js
@@ -1,21 +1,22 @@
-import React, { useEffect }  from 'react';
+import { useEffect }  from 'react';
 import { useAppContext } from '../../context/appContext';
 import {StatsContainer, Loading, ChartsContainer }from '../../components';
 
 const Stats = () => {
   const { showStats , isLoading, monthlyApplications } = useAppContext()
 
-  useEffect(()=>{
+ useEffect(()=>{
     showStats()
-  }, [])
-  if(isLoading ){
-    return <Loading center/>
+ }, [])
+
+  if (isLoading) {
+    return <Loading center />
   }
+
   return (
     <> 
-    <StatsContainer/>
-    {monthlyApplications.length > 0 && <ChartsContainer/>}
-   
+      <StatsContainer />
+      {monthlyApplications.length > 0 && <ChartsContainer />}  
     
      </>
   )
