82c5e06dd7f1cf65df5c8c8b711d2d0d3dedd7c6
Brahma, Jnyananjoy <jnybrahma@gmail.com>
2023-05-20 15:11:37 +1000

    added

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import 'express-async-errors'
 import morgan from 'morgan';
+import { dirname } from 'path'
+import {fileURLToPath } from 'url'
+import path from 'path'
+
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 const app = express()
 import dotenv from 'dotenv'
 dotenv.config()
     // db and authenticate User
 import connectDB from './db/connect.js'
+
 
 // routers
 
@@ -40,8 +47,18 @@ if (process.env.NODE_ENV !== 'production') {
     app.use(morgan('dev'))
 }
 app.use(express.json())
+
+// only when ready to deploy
+app.use(express.static(path.resolve(__dirname,'./client/build')))
+
 app.use('/api/v1/auth', authRouter)
 app.use('/api/v1/jobs', authenticateUser, jobsRouter)
+
+
+// only when ready to deploy 
+app.get('*', function(request, response) {
+	response.sendFile(path.resolve(__dirname,'./client/build','index.html'))
+})
 
 // middleware
 app.use(notFoundMiddleware)
